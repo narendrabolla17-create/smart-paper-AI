fcaef2a4a3556c19f018297b7ab829ef2bb92540
narendrabolla17-create <narendrabolla17@gmail.com>
2026-09-13 08:18:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-import sys
-import io
-
-# Force UTF-8 encoding to prevent UnicodeEncodeError on cloud servers
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
-
 import streamlit as st
 from google import genai
 
